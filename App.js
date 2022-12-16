ba4415fdbbb1f7b6db953a14492610760fc3d320
Sam MacFarlane <sam@Sams-MacBook-Air.local>
2022-12-16 16:52:30 +0000

    feat: Setup basic app

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import HomeScreen from "./Screens/HomeScreen";
+
+function DetailsScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "black",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 20,
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Welcome to the meditation app",
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,47 @@
+import { Button, View, Text, TouchableOpacity } from "react-native";
+
+import Card from "../Components/Card";
+
+const meditationEntries = [
+  {
+    date: "2021-08-01",
+    duration: 10,
+    rating: 5,
+    text: "I felt great after this meditation",
+  },
+  {
+    date: "2021-08-04",
+    duration: 10,
+    rating: 5,
+    text: "This meditation was a bit hit or miss but we move. Let's make this a long text to see how it looks",
+    type: "unguided",
+  },
+];
+
+function HomeScreen({ navigation }) {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-800 p-5">
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+      <TouchableOpacity
+        title="Begin meditation"
+        className="py-2 px-4 bg-blue-200 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+      >
+        <Text className="text-lg">Begin meditation</Text>
+      </TouchableOpacity>
+
+      {meditationEntries.map((entry) => (
+        <Card
+          title={entry.date}
+          text={entry.text}
+          duration={entry.duration}
+          type={entry?.type}
+        />
+      ))}
+    </View>
+  );
+}
+
+export default HomeScreen;
