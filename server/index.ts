d102b56bfd505022f70bed7516a6654b5a7149b3
Samuel MacFarlane <sammacfarlane23@icloud.com>
2024-01-06 13:56:24 +0000

    Fix Nodemon config and add some BE validation

--- server/index.ts
+++ server/index.ts
@@ -11,13 +11,14 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Load the /posts routes
 app.use("/entries", entries);
 
 // Global error handling
-// app.use((err, _req, res, next) => {
-//   res.status(500).send("Uh oh! An unexpected error occured.")
-// })
+app.use((err, req: Request, res: Response) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
+})
 
 // start the Express server
 app.listen(PORT, () => {
--- server/routes/entries.ts
+++ server/routes/entries.ts
@@ -1,5 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { validationResult, body, query, param } from "express-validator";
+
 import getDB from "../db/connect";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
@@ -12,14 +15,44 @@ router.get("/", async (req, res) => {
 });
 
 // Add a new entry to the collection
-router.post("/", async (req, res) => {
-  console.log("POST /entries");
-  const collection = await getDB().collection("entries");
-  // @TODO: Add validation
-  const newDocument = req.body;
-  const result = await collection.insertOne(newDocument);
-  console.log("successfully added entry to db", { result });
-  res.send(result).status(204);
-});
+router.post(
+  "/",
+  [
+    body("text", "Text must be string").optional().isString(),
+    body("duration", "Duration is required and must be numeric").isNumeric(),
+    body("date", "Date must be a valid date").isISO8601(),
+  ],
+  async (req: Request, res: Response) => {
+    const collection = await getDB().collection("entries");
+    const errors = validationResult(req);
+    const { text, duration, date } = req.body;
+
+    if (errors.isEmpty()) {
+      const newDocument = { text, duration, date };
+      const result = await collection.insertOne(newDocument);
+      console.log("successfully added entry to db", { result });
+      return res.send(result).status(204);
+    }
+    res.status(400).send({ errors: errors.array() });
+  }
+);
+
+// Delete an entry from the collection
+router.delete(
+  "/:id",
+  // @TODO: Get this working...
+  [param("id", "id must be a valid ObjectId").custom((input) => console.log({input})).isMongoId()],
+  async (req: Request, res: Response) => {
+    const collection = getDB().collection("posts");
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      let result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+      return res.send(result).status(200);
+    }
+
+    res.status(400).send({ errors: errors.array() });
+  }
+);
 
 export default router;
